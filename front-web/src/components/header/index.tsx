a388efecedd3b7a6c09a789104e84331d37c81fc
Andre Nunes <andre.totslaw@gmail.com>
2022-11-06 21:09:53 -0300

    Initial css setup & header component

---
+++ front-web/src/components/header/index.tsx
@@ -0,0 +1,11 @@
+import './styles.css';
+
+function Header() {
+  return (
+    <header className="main-header-container">
+      <h1 className="main-header-title">Desafio Aula 12 - Dashboard</h1>
+    </header>
+  );
+}
+
+export default Header;
